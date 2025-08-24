7c86b32238df84cba77be5bcfc970df88429833b
huryaq <hurryapurrya@gmail.com>
2025-08-24 23:45:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Raiden_Shogun_File_Bot")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://ibb.co/ycPCs3Ls")).split() #Required
+PICS = (os.environ.get("PICS", "https://ibb.co/v61mLHz5")).split() #Required
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
